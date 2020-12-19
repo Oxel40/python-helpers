bab28266a41729388bea483e42302a15ac0a58ca
Oxel40 <Oxel40@users.noreply.github.com>
2020-12-19 18:43:45 +0100

    remove comment from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="helpers",  # Replace with your own username
+    name="helpers",
     version="0.0.1",
     author="Oxel40",
     # author_email="author@example.com",
